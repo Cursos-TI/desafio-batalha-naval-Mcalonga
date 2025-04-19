10ac053b5c7172c8c51e39e731d5f50cd8049bec
Mcalonga <71896093+Mcalonga@users.noreply.github.com>
2025-04-19 20:21:28 +0000

    Nivel Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,20 +5,25 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    int matriz[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    int matriz[10][10] = {0};
+    char letras[11] = {' ','A','B','C','D','E','F','G','H','I','J'};
 
+
+    matriz[2][3]= 3;
+    matriz[2][4]= 3;
+    matriz[2][5]= 3;
+
+    matriz[6][3]= 3;
+    matriz[7][3]= 3;
+    matriz[8][3]= 3;
+
+    for(int n = 0; n < 11; n++){
+        printf("%c ",letras[n]);
+    }
+
+    printf("\n");
     for (int i = 0; i < 10; i++) {
+        printf("%d ",i+1);
         for (int j = 0; j < 10; j++) {
             printf("%d ", matriz[i][j]);
         }
