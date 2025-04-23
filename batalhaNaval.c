8beeab0ff026a05088d31f534929998e451679ce
Mcalonga <71896093+Mcalonga@users.noreply.github.com>
2025-04-23 20:26:10 +0000

    mudança .1

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,7 +4,31 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+void diagonalE(int matriz[][10], int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        for (int j = 0; j < tamanho; j++){
+            if(i == j){
+               matriz[i][j] = 3;
+            }
+        }
+    }
+}
+
+void diagonalD(int matriz[][10], int tamanho){
+    for(int i = 10; i > 0; i--){
+        for(int j = 10;j > 0; j--){
+            if(i + j == 9 && tamanho < 3){
+                matriz[i][j] = 3;
+                tamanho + 1;
+            }
+        }
+        
+    }
+}
+
 int main() {
+    int tamanho = 3;
+    int tamanho2 = 0;
     int matriz[10][10] = {0};
     char letras[11] = {' ','A','B','C','D','E','F','G','H','I','J'};
 
@@ -16,6 +40,9 @@ int main() {
     matriz[6][3]= 3;
     matriz[7][3]= 3;
     matriz[8][3]= 3;
+    
+    diagonalE(matriz, tamanho);
+    diagonalD(matriz, tamanho2);
 
     for(int n = 0; n < 11; n++){
         printf("%c ",letras[n]);
